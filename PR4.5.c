d7dd8a36b04f797bb0373876a618d60e3a8c7106
vrajgelani <vrajgelani1@gmail.com>
2025-05-23 09:36:30 +0530

    Add files via upload

---
+++ PR4.5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("        5        \n");
+	printf("      4 5 4      \n");
+	printf("    3 4 5 4 3    \n");
+	printf("  2 3 4 5 4 3 2  \n");
+	printf("1 2 3 4 5 4 3 2 1\n");
+}
